db96958a7acea5134a7be1cf4edcfb92121f1b7e
klkanika <kannika.klankhiew@gmail.com>
2023-01-16 16:43:31 +0700

    dd

--- packages/api/src/local.ts
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-import { configureApp, postRun } from "./app";
-import { initWebsocket } from "./websocket"
-const port = process.env.PORT || 9001;
-initWebsocket(configureApp().listen(port, () => {
-  postRun(port)
-}))
-console.log(`Listening on port ${port}`);
